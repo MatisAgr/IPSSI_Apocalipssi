d77649a7ebd8caec247bf76011bb2b5a7e5610d4
Tisma <matisagr@gmail.com>
2025-07-01 11:47:30 +0200

    ajout de la propriété 'resume' au schéma d'historique

--- back/models/historyModel.js
+++ back/models/historyModel.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const historySchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  action: { type: String, required: true }, 
+  action: { type: String, required: true },
+  resume: { type: Text, required: true },
   metadata: { type: Object },
   createdAt: { type: Date, default: Date.now }
 });
